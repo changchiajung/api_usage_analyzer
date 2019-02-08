3248f88c8af2dd9b5788afdeffd12efcffb85b84
changchiajung <42213793+changchiajung@users.noreply.github.com>
2019-02-08 17:41:17 +0800

    更新一點gui的東西
    gui.java裡面可以選擇directory 並輸出path
    可以拿到path就可以丟到ast那邊 作分析處理

--- src/gui.java
+++ src/gui.java
@@ -1,36 +1,61 @@
+
+import java.io.File;
+
 import javafx.application.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+
 import javafx.scene.layout.StackPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class gui extends Application{
-	
+public class gui extends Application {
+
 	Button button;
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	public void start(Stage primaryStage) throws Exception{
-		primaryStage.setTitle("Title of Demo");
-		
-		button = new Button();
-		button.setText("Click");
-		button.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				System.out.println("click button");
-			}});
-		
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
-		
-		Scene scene = new Scene(layout, 300, 250);
+
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("JavaFX App");
+
+		MenuBar menuBar = new MenuBar();
+
+		VBox vBox = new VBox(menuBar);
+		Menu menu = new Menu("File");
+		MenuItem menuItem1 = new MenuItem("Open");
+
+		menu.getItems().add(menuItem1);
+
+		menuBar.getMenus().add(menu);
+		menuItem1.setOnAction(e -> {
+			/*
+			 * FileChooser filechooser = new FileChooser();
+			 * filechooser.setTitle("choose file"); String string =
+			 * filechooser.showOpenDialog(null).getAbsolutePath();
+			 * System.out.println(string);
+			 */
+			DirectoryChooser directoryChooser = new DirectoryChooser();
+			directoryChooser.setTitle("Open Resource File");
+			File selectedDirectory = directoryChooser.showDialog(null);
+			if(selectedDirectory == null){
+			     //No Directory selected
+			}else{
+			     System.out.println(selectedDirectory.getAbsolutePath());
+			}
+		});
+		Scene scene = new Scene(vBox, 960, 600);
+
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
 	}
-	
+
 }
