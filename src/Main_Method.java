7c7bfd23ed3abe79507b05a26158ad71ffcf77d3
changchiajung <42213793+changchiajung@users.noreply.github.com>
2019-02-13 01:28:55 +0800

    db改動
    我改動了db的東西
    1.  現在db 只會在table不存在時重新create 也就是說不會覆蓋
    2. 承1. 每次的會放在同一個db
    3. 執行時 會新增client 進db
    4. 執行時 讀取每一個file會新增java module 進db
    to do
    1.  client新增時的client_name待確認
    2.  註解

--- src/Main_Method.java
+++ src/Main_Method.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -13,12 +14,15 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.ChildListPropertyDescriptor;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
@@ -34,7 +38,8 @@ import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 public class Main_Method {
-	// use ASTParse to parse string
+	 // use ASTParse to parse string
+	static String Client_ID ="";
 	public static void parse(String str) {
 		ASTParser parser = ASTParser.newParser(AST.JLS3);
 		parser.setResolveBindings(true);
@@ -43,7 +48,7 @@ public class Main_Method {
 		parser.setEnvironment( // apply classpath
 				new String[] { "C:\\Users\\User\\workspace\\winter_project\\bin" }, //
 				new String[] { "" }, new String[] { "UTF-8" }, true);
-		parser.setUnitName("winter_project/src/data.java"	);
+		parser.setUnitName("winter_project/src/data.java");
 
 		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
 
@@ -57,23 +62,24 @@ public class Main_Method {
 			 * public boolean visit(ParameterizedType node) { System.out.print("METHOD: " +
 			 * node.getName()); System.out.print(": "+node.getClass().getPackage());
 			 * System.out.println("Class: "+node.getClass()); return true; }
+			 * 
 			 */
 			public boolean visit(MethodInvocation node) {
 				System.out.println("METHOD Invocation: " + node.getName());
 				Expression expression = node.getExpression();
-				if (expression != null) {
-					//System.out.println("Expr: " + expression.toString());
+				if (expression != null) { //
+					System.out.println("Expr: " + expression.toString());
 					ITypeBinding typeBinding = expression.resolveTypeBinding();
 					if (typeBinding != null) {
 						System.out.println("Class: " + typeBinding.getName());
-						System.out.println("Package: "+ typeBinding.getPackage().getName()
-								);
+						System.out.println("Package: " + typeBinding.getPackage().getName());
 					}
 				}
-				IMethodBinding  method=node.resolveMethodBinding();
+				IMethodBinding method = node.resolveMethodBinding();
 				String method_string = method.toString();
-				System.out.println("parament type :" + method_string.substring(method_string.indexOf("(")+1,method_string.indexOf(")")));
-				
+				System.out.println("parament type :"
+						+ method_string.substring(method_string.indexOf("(") + 1, method_string.indexOf(")")));
+
 				return true;
 			}
 			/*
@@ -99,40 +105,82 @@ public class Main_Method {
 	}
 
 	// read file content into a string
-	public static String readFileToString(String filePath) throws IOException {
+	public static String readFileToString(String filePath) throws IOException, ClassNotFoundException, SQLException {
+		String filename = filePath.substring(filePath.lastIndexOf("\\")+1);
+		String filedirectory = filePath.substring(filePath.substring(0,filePath.lastIndexOf("\\")).lastIndexOf("\\")+1,filePath.lastIndexOf("\\"));
+		int java_module_id = 0 ;
+		Class.forName("org.sqlite.JDBC");
+		Connection connection = null;
+		connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+		Statement statement = connection.createStatement();
+		ResultSet rs_java_module_id = statement.executeQuery("select MAX(Java_Module_ID) as maxmodule from [JAVA MODULE]");
+		if (rs_java_module_id.next()) {
+			java_module_id = rs_java_module_id.getInt("maxmodule") + 1;
+		}
+		statement.executeUpdate("INSERT INTO [JAVA MODULE] values('" + java_module_id + "','" + filename + "','" + filedirectory + "','"+ Client_ID +"')");
+
 		StringBuilder fileData = new StringBuilder(1000);
 		BufferedReader reader = new BufferedReader(new FileReader(filePath));
 
 		char[] buf = new char[10];
 		int numRead = 0;
 		while ((numRead = reader.read(buf)) != -1) {
-			// System.out.println(numRead);
 			String readData = String.valueOf(buf, 0, numRead);
 			fileData.append(readData);
 			buf = new char[1024];
 		}
-
 		reader.close();
-
 		return fileData.toString();
 	}
 
 	// loop directory to get file list
-	public static void ParseFilesInDir() throws IOException {
+	public static void ParseFilesInDir() throws IOException, ClassNotFoundException, SQLException {
 		File dirs = new File(".");
 		String dirPath = dirs.getCanonicalPath() + File.separator + "src" + File.separator;
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		int w = 0;
+		int v = 0;
+		
+		
+		Class.forName("org.sqlite.JDBC");
+		Connection connection = null;
+		connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+		Statement statement = connection.createStatement();
+		try {
+			ResultSet rs_client_id = statement.executeQuery("select MAX(Client_ID) as maxclient from CLIENT");
+			if (rs_client_id.next()) {
+				w = rs_client_id.getInt("maxclient") + 1;
+				Client_ID = String.valueOf(w);
+			}
+			ResultSet rs_client_version = statement.executeQuery("select MAX(Client_version) as maxversion FROM CLIENT WHERE Client_name = 'src'");
+			if (rs_client_version.next()) {
+				v = rs_client_version.getInt("maxversion") + 1;
+			}
+			statement.executeUpdate("INSERT INTO CLIENT values('" + w + "','src','" + v + "','" + dateFormat.format(date) + "')");
+
+		} catch (SQLException e) {
+			System.out.println(e);
+			statement.executeUpdate(
+					"INSERT INTO CLIENT values('" + w + "','src','" + 1 + "','" + dateFormat.format(date) + "')");
+		}
 
 		File root = new File(dirPath);
-		// System.out.println(rootDir.listFiles());
 		File[] files = root.listFiles();
 		String filePath = null;
 		// read single file
 		parse(readFileToString("C:\\Users\\User\\workspace\\winter_project\\src\\data.java"));
 		/*
-		 * for (File f : files) { //System.out.println(f); filePath =
-		 * f.getAbsolutePath(); System.out.println(filePath); if (f.isFile()) {
-		 * parse(readFileToString(filePath)); } }
-		 */
+		for (File f : files) { // System.out.println(f); filePath =
+			filePath = f.getAbsolutePath();
+			System.out.println(filePath);
+			if (f.isFile()) {
+				parse(readFileToString(filePath));
+				//readFileToString(filePath);
+			}
+		}
+		*/
+
 	}
 
 	// create sqlite with four table
@@ -148,24 +196,15 @@ public class Main_Method {
 
 				Statement statement = connection.createStatement();
 				statement.setQueryTimeout(30); // set timeout to 30 sec.
-
-				statement.executeUpdate("DROP TABLE IF EXISTS API");
-				statement.executeUpdate("DROP TABLE IF EXISTS CLIENT");
-				statement.executeUpdate("DROP TABLE IF EXISTS [JAVA MODULE]");
-				statement.executeUpdate("DROP TABLE IF EXISTS [API USAGE]");
+				
 				statement.executeUpdate(
-						"CREATE TABLE [API] (API_ID STRING PRIMARY KEY, Package STRING NOT NULL, Class STRING NOT NULL, Method STRING, Field STRING)");
+						"CREATE TABLE IF NOT EXISTS [API] (API_ID STRING PRIMARY KEY NOT NULL, Package STRING NOT NULL, Class STRING NOT NULL, Method STRING, Field STRING)");
 				statement.executeUpdate(
-						"CREATE TABLE [CLIENT] (Client_ID STRING PRIMARY KEY, Client_name STRING NOT NULL, Client_Version STRING NOT NULL, Release_Data DATE NOT NULL)");
+						"CREATE TABLE IF NOT EXISTS [CLIENT] (Client_ID STRING PRIMARY KEY NOT NULL, Client_name STRING NOT NULL, Client_Version STRING NOT NULL, Release_Date DATE NOT NULL)");
 				statement.executeUpdate(
-						"CREATE TABLE [JAVA MODULE] (Java_Module_ID STRING PRIMARY KEY, Module name STRING NOT NULL, Module_Directory STRING NOT NULL, Client_ID STRING NOT NULL ,FOREIGN KEY(Client_ID) REFERENCES CLIENT(Client_ID))");
+						"CREATE TABLE IF NOT EXISTS [JAVA MODULE] (Java_Module_ID STRING PRIMARY KEY NOT NULL, Module_name STRING NOT NULL, Module_Directory STRING NOT NULL, Client_ID STRING ,foreign key(Client_ID) REFERENCES CLIENT(Client_ID))");
 				statement.executeUpdate(
-						"CREATE TABLE [API USAGE] (API_Usage_ID STRING PRIMARY KEY, Usage_Count INT NOT NULL, Client_ID STRING NOT NULL, API_ID STRING NOT NULL, Java_Module_ID STRING NOT NULL, FOREIGN KEY(Client_ID) REFERENCES CLIENT(Client_ID), FOREIGN KEY(API_ID) REFERENCES API(API_ID), FOREIGN KEY(Java_Module_ID) REFERENCES [JAVA MODULE](Java_Module_ID))");
-				/*
-				 * statement.executeUpdate("INSERT INTO person values(' "+ids[i]+"', '"+names[i]
-				 * +"')");
-				 */
-				// insert value in db
+						"CREATE TABLE IF NOT EXISTS [API USAGE] (API_Usage_ID STRING PRIMARY KEY NOT NULL, Usage_Count INT NOT NULL, Client_ID STRING , API_ID STRING , Java_Module_ID STRING, FOREIGN KEY(Client_ID) REFERENCES CLIENT(Client_ID), FOREIGN KEY(API_ID) REFERENCES API(API_ID), FOREIGN KEY(Java_Module_ID) REFERENCES [JAVA MODULE](Java_Module_ID))");
 			}
 
 			catch (SQLException e) {
@@ -181,7 +220,7 @@ public class Main_Method {
 		}
 	}
 
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
 		opendb();
 		ParseFilesInDir();
 	}
